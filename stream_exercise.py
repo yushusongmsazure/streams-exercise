3dcbdfec18a62bb862a9007c6d9ebb02b65a47a9
yushusongmsazure <yushusong@outlook.com>
2019-04-13 17:15:08 -0700

    finish up activity and setup virtualenv

--- stream_exercise.py
+++ stream_exercise.py
@@ -1,4 +1,5 @@
-
+#Yushu Song
+#Lesson02 Activity
 
 class StreamProcessor(object):
     """
@@ -47,7 +48,7 @@ class StreamProcessor(object):
 
     def process(self):
         """
-        TODO: Implement the `process` method, as described above.
+        Implement the `process` method, as described above.
         
         :return: int
         """
@@ -56,12 +57,12 @@ class StreamProcessor(object):
                    # together.
         total = 0  # The running total of sums.
 
-        # TODO: WRITE CODE HERE:
+        while count < 10 and total < 200:
+            digits = self._stream.read(2)
+            if(len(digits) < 2):
+                break
 
-        # Just some example syntax, you can read two digits from the head of the
-        # stream using the following code:
-        #
-        # digits = self._stream.read(2)
-
+            total += int(digits)
+            count += 1
 
         return count
